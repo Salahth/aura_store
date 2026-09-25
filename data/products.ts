4dff9a45fd89fae149136fe556ed78e21ab7eb99
Salahth <108548865+Salahth@users.noreply.github.com>
2026-09-25 14:57:08 +0100

    Refactor product data structure in products.ts

--- data/products.ts
+++ data/products.ts
@@ -17,19 +17,19 @@ export const products: Product[] = [
   price: 1600,
   desc: "شاحن سفري (Power Bank) بسعة 10000 مللي أمبير بتصميم مدمج وأنيق، مزود بشاشة LED رقمية لعرض مستوى البطارية ودعم منافذ إدخال وإخراج متعددة بقدرة 5V/2A.", //[span_1](start_span)[span_1](end_span)
   img: "https://i.ibb.co/cch6HhdT/1790343959227.jpg",
-}
+},
   {
   id: "airpods-hoco-vrac",
   name: "AirPods HOCO Vrac", //[cite: 3]
   price: 950,
   desc: "سماعات أذن لاسلكية أصلية من HOCO بدون علبة (Vrac)، باللون الأبيض وتأتي مع ضمان.", //[cite: 3]
   img: "https://i.ibb.co/dwzBCcd0/1790344363639.jpg",
-}
+},
   {
   id: "airpods-saiva-t50",
   name: "AirPods SAIVA T50",
   price: 2300,
   desc: "سماعات لاسلكية مريحة وثابتة مع اقتران تلقائي، وزمن شحن يدوم ساعتين، تأتي مع حافظة حماية سيليكون مقاومة للصدمات والغبار.",
   img: "https://i.ibb.co/WpkRZ39F/sans-titre-13-1-1.jpg",
-}
+},
 ];
